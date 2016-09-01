ff864786148d3aaf6038aa96005b275b620f53ea
uplooking <690050578@example.com>
2016-09-01 16:15:59 +0800

    哈哈

--- text/text/ViewController.m
+++ text/text/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // 哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
 }
 
 - (void)didReceiveMemoryWarning {
